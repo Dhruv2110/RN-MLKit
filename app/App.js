4532c2ea7378f4308b180bdc6f732a79dc1a7c72
Dhruv2110 <dhruvsharma.ynr@gmail.com>
2022-05-09 10:48:38 +0530

    Initial setup & app icon

---
+++ app/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+/* eslint-disable*/
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text style={{color:'black'}}>ML Kit</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
